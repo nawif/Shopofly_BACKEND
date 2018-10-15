bf2e6af4fa1be4c562f8347c7fd0b58a9414a35f
NAWIF <nnawif@gmail.com>
2018-10-15 20:18:52 +0300

    api reorganized

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,16 @@ Route::group([
     'prefix' => 'auth'
 
 ], function () {
-
+    Route::post('register','UserController@store');
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
 
+});
+Route::group([
+    'prefix' => 'user'
+
+], function () {
+    Route::post('register','UserController@store');
 });
