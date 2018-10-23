bdc556dc6f8096deea2e2211cd448a3d46669b06
NAWIF <nnawif@gmail.com>
2018-10-23 20:21:05 +0300

    updated the user schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->nullable();;
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('mobile_number')->unique();
+            $table->string('mobile_number')->unique()->nullable();;
             $table->longText('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
