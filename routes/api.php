12fbc57c62866c432d8769eea58d0de47d2d858a
NAWIF <nnawif@gmail.com>
2019-02-23 16:18:58 +0300

    code cleanup

--- routes/api.php
+++ routes/api.php
@@ -36,21 +36,19 @@ Route::group([
 ], function () {
     Route::post('register','SupplierController@store');
 });
+
 // Protected End points, (has to be logged in to access)
 Route::group(['middleware' => ['jwt.auth']],function(){
-Route::group([
-    'prefix' => 'store'
-
-], function () {
-
-    Route::post('addListing','ListingController@addListing');
-    Route::get('item/{key}','ListingController@getListing');
-    Route::get('SupplierItems/{id}','ListingController@getSupplierListing');
-    Route::post('addListingImages/{key}','ListingController@addListingImages');
-
+    Route::group([
+        'prefix' => 'store'
+    ], function () {
+        Route::post('addListing','ListingController@addListing');
+        Route::get('item/{key}','ListingController@getListing');
+        Route::get('SupplierItems/{id}','ListingController@getSupplierListing');
+        Route::post('addListingImages/{key}','ListingController@addListingImages');
         Route::post('checkout','OrderController@checkOut');
-    });
-
+        }
+    );
 });
 
 
