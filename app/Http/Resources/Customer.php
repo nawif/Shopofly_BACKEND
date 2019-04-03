03429c693c2d0c32f4f12515102b483a241b032b
Nawaf Alquaid <nnawif@gmail.com>
2019-04-03 22:11:04 +0300

    added user detail in order response

---
+++ app/Http/Resources/Customer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Customer extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+
+        return [
+            'full_name' => $this->name,
+            'mobile_number' => $this->mobile_number
+        ];
+    }
+}
--- app/Http/Resources/Order.php
+++ app/Http/Resources/Order.php
@@ -3,6 +3,10 @@
 namespace App\Http\Resources;
 use App\Http\Resources\Listing as ListingResource;
 use App\Http\Resources\Address as AddressResource;
+use App\Http\Resources\Customer as CustomerResource;
+
+
+
 
 
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -20,7 +24,8 @@ class Order extends JsonResource
         return [
             "items" => ListingResource::collection($this->listings),
             "total" => $this->getBill(),
-            "address" => new AddressResource($this->address)
+            "address" => new AddressResource($this->address),
+            "customer" => new CustomerResource($this->user)
         ];
     }
 }
